344965e0b0a13f304f57abecfc4b04a730a7b5d0
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2021-05-13 18:39:31 +0700

    feat: Code for Tizen added. Usability unconfirmed.

---
+++ ToastNotification.Tizen/README.md
@@ -0,0 +1,6 @@
+# ToastNotification.Uno.Tizen  
+
+To use notifications, the application has to request the `notification` permission in Tizen.
+This can be done by adding `http://tizen.org/privilege/notification` to `tizen-manifest.xml`.
+
+For more details study [this documentation](https://docs.tizen.org/application/dotnet/guides/notification/noti/#prerequisites).
---
+++ ToastNotification.Tizen/ToastNotificationImplementation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tizen.Applications.Notifications;
+
+namespace Uno.Extras
+{
+    public static class ToastNotificationImplementation
+    {
+        /// <summary>
+        /// Shows a native Tizen Quick Panel notification.
+        /// </summary>
+        /// <param name="toast">ToatsNotification as created in the native library.</param>
+        /// <returns></returns>
+        public static async Task Show(this ToastNotification toast)
+        {
+            var notification = new Notification()
+            {
+                Title = toast.Title,
+                Content = toast.Message,
+            };
+
+            if (toast.AppLogoOverride != null)
+            {
+                var file = await toast.AppLogoOverride.GetStorageFileAsync().ConfigureAwait(false);
+                notification.Icon = file.Path;
+            }
+
+            NotificationManager.Post(notification);
+        }
+    }
+}
