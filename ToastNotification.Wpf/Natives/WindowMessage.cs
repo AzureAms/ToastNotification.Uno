6b4a05ebe171a8d7470b82d28a585d75695e360a
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2021-07-26 13:35:35 +0700

    feat(WPF): Added support for notification clicked event

---
+++ ToastNotification.Wpf/Natives/WindowMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notification.Natives
+{
+    internal enum WindowMessage
+    {
+        User = 0x0400,
+        App = 0x0800
+    }
+}
--- ToastNotification.Wpf/ToastNotificationImplementation.cs
+++ ToastNotification.Wpf/ToastNotificationImplementation.cs
@@ -13,7 +13,25 @@ namespace Uno.Extras
     public static class ToastNotificationImplementation
     {
         private static readonly object dummy = new object();
+        private static readonly SemaphoreSlim semaphore = new SemaphoreSlim(1);
         private static Guid id = id = Guid.NewGuid();
+
+        private static uint _callbackMessage = (uint)WindowMessage.App + 1;
+        /// <summary>
+        /// The CallbackMessage, which is an application-defined value.
+        /// The value MUST be between WindowMessage.App and 0xBFFF
+        /// </summary>
+        public static uint CallbackMessage
+        {
+            get => _callbackMessage;
+            set
+            {
+                if (Math.Min(Math.Max((uint)WindowMessage.App, value), 0xBFFFu) != value)
+                {
+                    throw new ArgumentOutOfRangeException("Callback message code MUST be between WindowMessage.App and 0xBFFF");
+                }
+            }
+        }
 
         /// <summary>
         /// Shows the toast notification using native Win32 APIs.
@@ -24,7 +42,8 @@ namespace Uno.Extras
             Icon icon;
             if (toast.AppLogoOverride != null)
             {
-                var imageStream = await toast.AppLogoOverride.GetStreamAsync().ConfigureAwait(false);
+                // For some reasons, this must be done on the same Thread.
+                var imageStream = await toast.AppLogoOverride.GetStreamAsync().ConfigureAwait(true);
                 var bitmap = (Bitmap)Image.FromStream(imageStream);
                 icon = Icon.FromHandle(bitmap.GetHicon());
             }
@@ -33,42 +52,65 @@ namespace Uno.Extras
                 icon = GetProcessIcon();
             }
 
-            lock (dummy)
+            var tcs = new TaskCompletionSource<object>();
+
+            await semaphore.WaitAsync();
+
+            var notifyManager = new NotificationManagerWindow(CallbackMessage);
+
+            var notifyData = new NotificationIconData();
+            notifyData.hWnd = notifyManager.Handle;
+            notifyData.cbSize = (uint)Marshal.SizeOf(notifyData);
+            notifyData.uCallbackMessage = CallbackMessage;
+            notifyData.DUMMYUNIONNAME_uTimeout_uVersion = 0x4;
+            notifyData.hIcon = GetProcessIcon().Handle;
+            notifyData.szTip = string.Empty;
+            notifyData.uFlags = NotificationIconFlags.Icon | NotificationIconFlags.Tip | NotificationIconFlags.Guid | NotificationIconFlags.Message;
+            notifyData.guidItem = id;
+
+            if (!notifyData.AddIcon())
             {
-                var notifyData = new NotificationIconData();
-                notifyData.hWnd = Process.GetCurrentProcess().MainWindowHandle;
-                notifyData.cbSize = (uint)Marshal.SizeOf(notifyData);
-                notifyData.DUMMYUNIONNAME_uTimeout_uVersion = 0x4;
-                notifyData.hIcon = GetProcessIcon().Handle;
-                notifyData.szTip = string.Empty;
-                notifyData.uFlags = NotificationIconFlags.Icon | NotificationIconFlags.Tip | NotificationIconFlags.Guid;
-                notifyData.guidItem = id;
-
-                notifyData.AddIcon();
-
-                notifyData.uFlags = NotificationIconFlags.Info | NotificationIconFlags.Icon | NotificationIconFlags.Guid;
-                notifyData.szInfoTitle = toast.Title;
-                notifyData.szInfo = toast.Message;
-                notifyData.dwInfoFlags = NotificationIconInfoFlags.User | NotificationIconInfoFlags.LargeIcon;
-                notifyData.hBalloonIcon = icon.Handle;
-
-                Shell.NotifyIcon(NotificationIconMessage.Modify, notifyData);
-
-                // Extra 1500ms for the notification to fade out:
-                Thread.Sleep(GetNotificationDuration() * 1000 + 1500);
-                notifyData.RemoveIcon();
+                throw new InvalidOperationException("Cannot add icon.");
             }
+
+            notifyData.uFlags = NotificationIconFlags.Info | NotificationIconFlags.Icon | NotificationIconFlags.Guid;
+            notifyData.szInfoTitle = toast.Title;
+            notifyData.szInfo = toast.Message;
+            notifyData.dwInfoFlags = NotificationIconInfoFlags.User | NotificationIconInfoFlags.LargeIcon;
+            notifyData.hBalloonIcon = icon.Handle;
+
+            notifyManager.BalloonHide += (s, a) =>
+            {
+                notifyData.RemoveIcon();
+                tcs.SetResult(null);
+            };
+
+            notifyManager.BallonClicked += (s, a) =>
+            {
+                notifyData.RemoveIcon();
+                tcs.SetResult(null);
+                toast.RaisePressed(EventArgs.Empty);
+            };
+
+            Shell.NotifyIcon(NotificationIconMessage.Modify, notifyData);
+
+            await tcs.Task.ConfigureAwait(true);
+
+            // The Window must be disposed on the same Thread.
+            notifyManager.Dispose();
+
+            semaphore.Release();
         }
 
-        private static void AddIcon(this NotificationIconData notifyData)
+        private static bool AddIcon(this NotificationIconData notifyData)
         {
-            Shell.NotifyIcon(NotificationIconMessage.Add, notifyData);
-            Shell.NotifyIcon(NotificationIconMessage.SetVersion, notifyData);
+            return Shell.NotifyIcon(NotificationIconMessage.Add, notifyData) 
+                && Shell.NotifyIcon(NotificationIconMessage.SetVersion, notifyData);
         }
 
-        private static void RemoveIcon(this NotificationIconData notifyData)
+        private static bool RemoveIcon(this NotificationIconData notifyData)
         {
-            Shell.NotifyIcon(NotificationIconMessage.Delete, notifyData);
+            return Shell.NotifyIcon(NotificationIconMessage.Delete, notifyData);
         }
 
         /// <summary>
@@ -88,7 +130,5 @@ namespace Uno.Extras
             Icon icon = Icon.ExtractAssociatedIcon(Assembly.GetEntryAssembly().Location);
             return icon;
         }
-
-  
     }
 }
