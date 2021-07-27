c7cb934a75f5cb90ec54fdf6b9099ab53ab4fbf3
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2021-07-27 23:30:39 +0700

    feat: Added Actions to Gtk

--- ToastNotification.Gtk/ToastNotificationImplementation.cs
+++ ToastNotification.Gtk/ToastNotificationImplementation.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading;
+using Windows.ApplicationModel.Activation;
+using Windows.System;
 using Task = System.Threading.Tasks.Task;
 using Thread = System.Threading.Thread;
 
@@ -12,6 +14,11 @@ namespace Uno.Extras
     {
         private static Dictionary<Guid, ToastNotification> notifications = new Dictionary<Guid, ToastNotification>();
         private static Dictionary<ToastNotification, Guid> ids = new Dictionary<ToastNotification, Guid>();
+
+        private const string LaunchFromNotificationForeground = "LaunchFromNotificationForeground";
+        private const string LaunchFromNotificationBackground = "LaunchFromNotificationBackground";
+        private const string LaunchProtocolFromNotification = "LaunchProtocolFromNotification";
+        private const string DismissAction = "DismissNotification";
 
         /// <summary>
         /// Shows a native Gtk notification. For best results, you shows initialize a 
@@ -58,6 +65,17 @@ namespace Uno.Extras
                 notifications.Add(id, toast);
             }
 
+            notification.SetDefaultActionAndTargetValue("app." + LaunchFromNotificationForeground, new Variant(toast.Arguments));
+            
+            if (toast.ToastButtons != null)
+            {
+                foreach (var button in toast.ToastButtons)
+                {
+                    notification.AddButtonWithTargetValue(button.Content, button.GetAppropriateAction(),
+                        new Variant(button.ActivationType != ToastActivationType.Protocol ? button.Arguments : button.Protocol.ToString()));
+                }
+            }
+
             await InvokeAsync(() =>
             {
                 if (Application.Default == null)
@@ -80,6 +98,77 @@ namespace Uno.Extras
                     File.Delete(path);
                 }
             }).ConfigureAwait(false);
+        }
+
+        // On GTK, we don't know how many buttons notifications support.
+        public static int GetButtonLimit(this ToastNotification toast) => -1;
+
+        static ToastNotificationImplementation()
+        {
+            var foregroundAction = new SimpleAction(LaunchFromNotificationForeground, VariantType.String);
+            var backgroundAction = new SimpleAction(LaunchFromNotificationBackground, VariantType.String);
+            var protocolAction = new SimpleAction(LaunchProtocolFromNotification, VariantType.String);
+            var dismissAction = new SimpleAction(DismissAction, VariantType.Any);
+
+            foregroundAction.Activated += ForegroundAction_Activated;
+            backgroundAction.Activated += BackgroundAction_Activated;
+            protocolAction.Activated += ProtocolAction_Activated;
+
+            var app = Application.Default;
+
+            app.AddAction(foregroundAction);
+            app.AddAction(backgroundAction);
+            app.AddAction(protocolAction);
+            app.AddAction(dismissAction);
+        }
+
+        private static void ForegroundAction_Activated(object o, ActivatedArgs args)
+        {
+            var arguments = (string)args.P0;
+            ActivateApp(arguments);
+        }
+
+        private static void BackgroundAction_Activated(object o, ActivatedArgs args)
+        {
+            var arguments = (string)args.P0;
+            ActivateBackground(arguments);
+        }
+
+        private static void ProtocolAction_Activated(object o, ActivatedArgs args)
+        {
+            var protocol = (string)args.P0;
+            _ = Launcher.LaunchUriAsync(new Uri(protocol));
+        }
+
+        private static void ActivateApp(string argument)
+        {
+            var app = Windows.UI.Xaml.Application.Current;
+            var args = Reflection.Construct<ToastNotificationActivatedEventArgs>(argument);
+            app.Invoke("OnActivated", new object[] { args });
+        }
+
+        private static void ActivateBackground(string argument)
+        {
+            throw new NotImplementedException("Uno Platform does not support background tasks");
+        }
+
+        private static string GetAppropriateAction(this ToastButton button)
+        {
+            if (button.ShouldDissmiss)
+            {
+                return DismissAction;
+            }
+            switch (button.ActivationType)
+            {
+                case ToastActivationType.Background:
+                    return LaunchFromNotificationBackground;
+                case ToastActivationType.Foreground:
+                    return LaunchFromNotificationForeground;
+                case ToastActivationType.Protocol:
+                    return LaunchProtocolFromNotification;
+                default:
+                    throw new ArgumentOutOfRangeException("ToastActivationType does not exist!");
+            }
         }
 
         private static Task InvokeAsync(Action a)
