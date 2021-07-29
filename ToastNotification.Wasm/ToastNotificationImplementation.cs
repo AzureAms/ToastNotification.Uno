fbee1f7e31848cf60c7790a0928accd60539ed19
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2021-07-29 10:18:30 +0700

    refactor: Moved Notification WASM Implementation to TypeScript

--- ToastNotification.Wasm/ToastNotificationImplementation.cs
+++ ToastNotification.Wasm/ToastNotificationImplementation.cs
@@ -11,46 +11,9 @@ namespace Uno.Extras
 {
     public static class ToastNotificationImplementation
     {
-        private static readonly string AskNotificationPermission =
-$@"
-(function askNotificationPermission() {{
-    const reportStatus = Module.mono_bind_static_method('[{Assembly.GetExecutingAssembly().GetName().Name}] {typeof(ToastNotificationImplementation).FullName}:Report');
-    function handlePermission(permission) {{
-        // set the button to shown or hidden, depending on what the user answers
-        reportStatus(Notification.permission.toString());
-    }}
-    
-    // Check Notification.requestPermission's syntax
-    function checkNotificationPromise() {{
-        try {{
-            Notification.requestPermission().then();
-        }} catch (e) {{
-            return false;
-        }}
-
-        return true;
-    }}
-
-    // Let's check if the browser supports notifications
-    if (!('Notification' in window)) {{
-        reportStatus('FeatureNotSupported');
-    }} else {{
-        if (checkNotificationPromise()) {{
-            Notification.requestPermission()
-                .then((permission) => {{
-                    handlePermission(permission);
-                }})
-        }} else {{
-            Notification.requestPermission(function (permission) {{
-                handlePermission(permission);
-            }});
-        }}
-    }}
-}})();
-";
+        private const string JsType = "Uno.Extras.ToastNotificationImplementation";
 
         private static bool? _permission;
-        private static TaskCompletionSource<bool> _tcs;
 
         /// <summary>
         /// Tries to get the relevant permissions and show toast notifications.
@@ -59,18 +22,15 @@ $@"
         /// <returns></returns>
         public static async Task Show(this ToastNotification toast)
         {
-            // Mono seems to optimize away unsued functions..
-            if (await DummyFalseStuff().ConfigureAwait(false))
+            if (_permission != true)
             {
-                Report(null);
+                _permission = await QueryPermissionAsync().ConfigureAwait(false);
             }
 
-            _permission = _permission ?? await QueryPermissionAsync().ConfigureAwait(false);
-            
             if ((bool)_permission)
             {
-                WebAssemblyRuntime.InvokeJS($@"
-                    var n = new Notification(
+                await WebAssemblyRuntime.InvokeAsync($@"
+                    {JsType}.Show(
                         '{WebAssemblyRuntime.EscapeJs(toast.Title)}',
                         {{
                             body: '{WebAssemblyRuntime.EscapeJs(toast.Message)}',
@@ -84,12 +44,6 @@ $@"
                 // Fall back to simple alert pop-up.
                 WebAssemblyRuntime.InvokeJS($@"alert('{WebAssemblyRuntime.EscapeJs(toast.Message)}');");
             }
-        }
-
-        private static async Task<bool> DummyFalseStuff()
-        {
-            await Task.Delay(1).ConfigureAwait(false);
-            return false;
         }
 
         private static async Task<string> SetIconIfOveriddenAsync(LogoSource image, bool comma = false)
@@ -111,45 +65,35 @@ $@"
                 data = uri.ToString();
             }
 
-            return $"icon: '{data}'" + (comma ? "," : string.Empty);
+            return $"icon: '{WebAssemblyRuntime.EscapeJs(data)}'" + (comma ? "," : string.Empty);
         }
 
         /// <summary>
         /// Queries the permission to send notifications from the user.
         /// </summary>
         /// <returns></returns>
-        public static Task<bool> QueryPermissionAsync()
+        public static async Task<bool> QueryPermissionAsync()
         {
-            if (_tcs != null)
-            {
-                return _tcs.Task;
-            }
+            var permissionStatus = await WebAssemblyRuntime.InvokeAsync($"{JsType}.QueryPermissionAsync()");
 
-            _tcs = new TaskCompletionSource<bool>();
-            WebAssemblyRuntime.InvokeJS(AskNotificationPermission);
-            return _tcs.Task;
-        }
-
-        private static void Report(string status)
-        {
-            switch (status)
+            switch (permissionStatus)
             {
                 case "FeatureNotSupported":
-                    System.Diagnostics.Debug.WriteLine("Feature is not supported on this browser");
-                    _tcs.SetResult(false);
-                    break;
+                    Debug.WriteLine("Feature is not supported on this browser");
+                    return false;
                 case "denied":
                 case "default":
-                    _tcs.SetResult(false);
-                    break;
+                    return false;
                 case "granted":
-                    _tcs.SetResult(true);
-                    break;
+                    return true;
                 default:
-                    _tcs.SetResult(false);
-                    break;
+                    return false;
             }
-            _tcs = null;
+        }
+
+        public static int GetButtonLimit(this ToastNotification toast)
+        {
+            return int.Parse(WebAssemblyRuntime.InvokeJS($"{JsType}.GetButtonLimit()"));
         }
     }
 }
---
+++ ToastNotification.Wasm/ts/Uno.Extras.ToastNotificationImplementation.ts
@@ -0,0 +1,88 @@
+﻿declare const config: any;
+declare const Module: any;
+
+namespace Uno.Extras {
+    export class ToastNotificationImplementation {
+        private static ServiceWorkerName = "Notifications.ServiceWorker.js";
+        private static _registered = false;
+        private static _serviceWorker: ServiceWorkerRegistration;
+
+        private static GetScriptsLocation() : string {
+            var scriptLocation = "";
+
+            var unoAppBase = config.uno_app_base;
+
+            // Using some Uno Platform internal variables.
+            if (typeof (unoAppBase) == "string") {
+                scriptLocation += "./" + unoAppBase;
+            }
+            // A fallback using the Document.
+            else {
+                scriptLocation += Array
+                    .from(document.scripts)
+                    .map(script => script.baseURI + script.attributes.getNamedItem("src").value)
+                    .find(name => name.endsWith("dotnet.js"))
+                    .match(/([\S\s]*package_[a-zA-Z0-9]*)/g)[0];
+            }
+
+            scriptLocation += "/";
+
+            return scriptLocation;
+        }
+
+        private static async RegisterServiceWorkerAsync(): Promise<ServiceWorkerRegistration> {
+            var location = this.GetScriptsLocation();
+
+            return await navigator.serviceWorker.register(location + this.ServiceWorkerName);
+        }
+
+        public static async Show(title: string, options: NotificationOptions): Promise<void> {
+            if (!this._registered) {
+                this._serviceWorker = await this.RegisterServiceWorkerAsync();
+            }
+
+            await this._serviceWorker.showNotification(title, options);
+        }
+
+        public static QueryPermissionAsync(): Promise<string> {
+
+            return new Promise<string>(resolve => {
+                function handlePermission(permission: NotificationPermission) {
+                    resolve(permission.toString());
+                }
+
+                // Check Notification.requestPermission's syntax
+                function checkNotificationPromise() {
+                    try {
+                        Notification.requestPermission().then();
+                    }
+                    catch (e) {
+                        return false;
+                    }
+                    return true;
+                }
+
+                // Let's check if the browser supports notifications
+                if (!('Notification' in window)) {
+                    resolve('FeatureNotSupported');
+                }
+                else {
+                    if (checkNotificationPromise()) {
+                        Notification.requestPermission().then((permission) => {
+                            handlePermission(permission);
+                        })
+                    }
+                    else {
+                        Notification.requestPermission(function (permission) {
+                            handlePermission(permission);
+                        });
+                    }
+                }
+            });
+        }
+
+        public static GetButtonLimit(): Number {
+            return Notification.maxActions;
+        }
+    }
+}
